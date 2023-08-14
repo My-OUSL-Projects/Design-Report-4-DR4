a24b293d0e5d11a144b4735986d92aac2a48ceb9
Angelo Perera <s92062815@ousl.lk>
2023-08-14 07:19:00 +0530

    updating

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Register from "./screens/RegisterScr";
 import Login from "./screens/LoginScr";
