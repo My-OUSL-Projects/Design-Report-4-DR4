dadea6530fd97971e944e3aaa028b73d8e380dbd
Angelo Perera <s92062815@ousl.lk>
2023-08-18 08:06:09 +0530

    updating

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,9 +1,9 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import Register from "./screens/RegisterScr";
 import Login from "./screens/LoginScr";
 import Home from "./screens/HomeScr";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
